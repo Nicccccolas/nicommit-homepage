67a1d2ea857bbf617359bbda2a59af9c6bf03ac5
Nicolas <nicolaspantojadi@gmail.com>
2024-01-31 16:31:13 -0500

    feat bio section with tech stack

---
+++ components/techstack.js
@@ -0,0 +1,49 @@
+import {
+  Box,
+  Flex,
+  SimpleGrid,
+  Text,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { IoLogoReact, IoLogoNodejs, IoLogoDocker } from 'react-icons/io5'
+import { BiLogoPostgresql, BiLogoMongodb, BiLogoAws } from 'react-icons/bi'
+import { TbBrandNextjs } from 'react-icons/tb'
+import { SiExpress, SiChakraui } from 'react-icons/si'
+
+const TechStack = () => {
+  const techItems = [
+    { icon: IoLogoNodejs, name: 'NODE' },
+    { icon: SiExpress, name: 'EXPRESS' },
+    { icon: BiLogoPostgresql, name: 'POSTGRESQL' },
+    { icon: BiLogoMongodb, name: 'MONGODB' },
+    { icon: IoLogoReact, name: 'REACT' },
+    { icon: TbBrandNextjs, name: 'NEXT' },
+    { icon: SiChakraui, name: 'CHAKRAUI' },
+    { icon: BiLogoAws, name: 'AWS' },
+    { icon: IoLogoDocker, name: 'DOCKER' }
+  ]
+
+  return (
+    <SimpleGrid columns={[2, 2, 3]} pt={4} gap={[2, 4, 4]} align="center">
+      {techItems.map((tech, index) => (
+        <Box
+          key={index}
+          border="0.5px dashed rgba(238, 69, 64, 0.3)"
+          borderRadius="md"
+          p={3}
+          bg={useColorModeValue('#ffffff40', '#51013280')}
+          style={{ backdropFilter: 'blur(10px)' }}
+          _hover={{
+            bg: useColorModeValue('#ffffff80', '#510132b3'),
+            transform: 'scale(1.05)'
+          }}
+        >
+          <tech.icon fontSize="2em" />
+          <Text mt={2}>{tech.name}</Text>
+        </Box>
+      ))}
+    </SimpleGrid>
+  )
+}
+
+export default TechStack
--- pages/bio.js
+++ pages/bio.js
@@ -8,6 +8,7 @@ import Section from '../components/section'
 import { BioSection, BioYear } from '../components/bio'
 import Layout from '../components/layouts/article'
 import Paragraph from '../components/paragraph'
+import TechStack from '../components/techstack'
 
 const Bio = () => {
   return (
@@ -46,6 +47,9 @@ const Bio = () => {
             Working as a freelancer.
           </BioSection>
         </Section>
+
+        <Divider my={4} />
+
         <Section delay={0.2}>
           <Section delay={0.3}>
             <Heading as="h3" variant="section-title">
@@ -61,6 +65,17 @@ const Bio = () => {
               </Link>
               , Playing Turntable, Concerts and Festivals, Cybersecurity.
             </Paragraph>
+          </Section>
+
+          <Divider my={4} />
+
+          <Section delay={0.2}>
+            <Section delay={0.3}>
+              <Heading as="h3" variant="section-title">
+                Tech Stack
+              </Heading>
+              <TechStack />
+            </Section>
           </Section>
         </Section>
       </Container>
