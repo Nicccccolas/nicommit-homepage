97d0b75aed6a4fe6aeccb358b57a18dff6335e03
Nicolas <nicolaspantojadi@gmail.com>
2024-02-05 10:05:39 -0500

    feat add open graph section in main

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -8,9 +8,14 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="author" content="Nicolás Pantoja Díaz" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <title>Nicccccolas - Homepage</title>
+        <meta property="og:site_name" content="Nicolás Pantoja Díaz" />
+        <meta name="og:title" content="Nicolas Pantoja Díaz" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/images/logo3.png" />
+        <title>Nicolás Pantoja Díaz - Homepage</title>
       </Head>
       <Navbar path={router.asPath} />
       <Container maxW="container.xl" pt={20}>
